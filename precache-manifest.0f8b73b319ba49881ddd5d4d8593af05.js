5b66d6012b91f13b6d47012546fb7acb05d79f23
sameer486 <ryadav.rwr@gmail.com>
2020-11-23 10:42:07 +0530

    Updates

---
+++ precache-manifest.0f8b73b319ba49881ddd5d4d8593af05.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c0870ab223811bc07166b46a71c5fe",
+    "url": "/MyTube/index.html"
+  },
+  {
+    "revision": "f2c1799f01ae4bc2b6ba",
+    "url": "/MyTube/static/css/2.5e9fa3f8.chunk.css"
+  },
+  {
+    "revision": "135fa187cdd01079b554",
+    "url": "/MyTube/static/css/main.5bdc4c59.chunk.css"
+  },
+  {
+    "revision": "f2c1799f01ae4bc2b6ba",
+    "url": "/MyTube/static/js/2.49ef5215.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/MyTube/static/js/2.49ef5215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135fa187cdd01079b554",
+    "url": "/MyTube/static/js/main.10a1be83.chunk.js"
+  },
+  {
+    "revision": "e81a131c7327b8e05ee2",
+    "url": "/MyTube/static/js/runtime-main.4c7bd77a.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/MyTube/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/MyTube/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/MyTube/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/MyTube/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/MyTube/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/MyTube/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/MyTube/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/MyTube/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/MyTube/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/MyTube/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/MyTube/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e5520ae279d2f04880c75932b3e60ded",
+    "url": "/MyTube/static/media/logo.e5520ae2.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/MyTube/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/MyTube/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/MyTube/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/MyTube/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/MyTube/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
